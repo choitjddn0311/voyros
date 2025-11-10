fdc96b33a91addc5f939155a36a0f140f351e711
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2025-11-10 20:32:18 +0900

    refactor: file remake

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import './style/tailwind.css';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-200">
-      <h1 className="text-5xl font-bold text-blue-500">
-        Tailwind CSS 적용 완료! 🎉
-      </h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
