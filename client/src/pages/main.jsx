4d770133d27cda6d99892ea0334c594c6d8d590f
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2025-11-11 23:46:25 +0900

    feat: make file mainPage

---
+++ client/src/pages/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainHome = () => {
+    return (
+        <>
+            <h1>hello. welcome to the Voyros. </h1>
+        </>
+    )
+}
+
+export default MainHome;
