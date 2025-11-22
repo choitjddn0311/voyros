00bdfde41380bd0f5c69e3d1229551cb213298ef
choitjddn0311 <cswcjw311427@naver.com>
2025-11-22 16:19:06 +0900

    chore: header 구조 변경 및 일부 수정사항 추가

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -5,9 +5,26 @@ import MainLogo from "../assets/imgs/logo/logo_350.png";
 
 const Util = styled.div`
     width: 100%;
-    height: 20px;
-    background: red;
-`
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const UtilInner = styled.div`
+    width: 1400px;
+    height: 100%;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+`;
+
+const Ul = styled.ul`
+    display: flex;
+    justify-content: end;
+    gap: 20px;
+    
+`;
 
 const MainHeader = styled.header`
     width: 100%;
@@ -15,14 +32,31 @@ const MainHeader = styled.header`
     display: flex;
     justify-content: center;
     align-items: center;
-`
+`;
+
+const HeaderInner = styled.div`
+    width: 1400px;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
 
 const Header = () => {
     return (
         <>
-            <Util></Util>
+            <Util>
+                <UtilInner>
+                    <Ul>
+                        <li><a href="#">로그인</a></li>
+                        <li><a href="#">회원가입</a></li>
+                    </Ul>
+                </UtilInner>
+            </Util>
             <MainHeader>
-                <img src={MainLogo} alt="" />
+                <HeaderInner>
+                    <img src={MainLogo} alt="" />
+                </HeaderInner>
             </MainHeader>
         </>
     )
