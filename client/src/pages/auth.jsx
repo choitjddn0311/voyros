194dab2b585d2e906453bbee023294cce13a9651
choitjddn0311 <cswcjw311427@naver.com>
2025-11-23 01:32:56 +0900

    chore: 기본 구조 완료

--- client/src/pages/auth.jsx
+++ client/src/pages/auth.jsx
@@ -1,9 +1,52 @@
 import React from "react";
 import styled from "styled-components";
 
+const MainContainer = styled.main`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #fff;
+`
+
+const FormContainer = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const AuthForm = styled.form`
+    width: 500px;
+    height: 650px;
+    background: #fefefe;
+    box-shadow: 0 0 20px #eee,
+                0 0 40px #efefef;
+    border-radius: 10px ;
+`;
+
+const AuthIntro = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
 const AuthPage = () => {
     return (
-        <h1>안녕하세요. 로그인하세요</h1>
+        <>
+            <MainContainer>
+                <FormContainer>
+                    <AuthForm>
+
+                    </AuthForm>
+                </FormContainer>
+                <AuthIntro />
+            </MainContainer>
+        </>
     )
 }
 
