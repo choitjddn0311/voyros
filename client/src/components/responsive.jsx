5ad837028a7e68052abba6acc6e178a6a1f2ee29
choitjddn0311 <cswcjw311427@naver.com>
2025-11-22 19:52:01 +0900

    feat: tablet 반응형 추가 및 반응형 코드 스타일 수정

--- client/src/components/responsive.jsx
+++ client/src/components/responsive.jsx
@@ -1,18 +1,15 @@
 import {useMediaQuery} from 'react-responsive';
 import React from "react";
 
-export const MoblieHeader = ({children}) => {
-    const isMoblie = useMediaQuery({
-        query: "(max-width: 800px)"
-    });
-
+export const Moblie = ({children}) => {
+    const isMoblie = useMediaQuery({ maxWidth: 767});
     return <>{isMoblie && children}</>
-}
-
-export const PcHeader = ({children}) => {
-    const isPc = useMediaQuery({
-        query: "(min-width: 801px)"
-    });
-
+};
+export const Tablet = ({children}) => {
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1200});
+    return <>{isTablet && children}</>
+};
+export const Desktop = ({children}) => {
+    const isPc = useMediaQuery({ minWidth: 1201});
     return <>{isPc && children}</>;
-}
+};
