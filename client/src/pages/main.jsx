ed4170db23b885d2a7cb0ff7935e647dffdf0b88
Sungwoo Choi <128563032+choitjddn0311@users.noreply.github.com>
2025-11-28 23:13:00 +0900

    chore: main intro create

--- client/src/pages/main.jsx
+++ client/src/pages/main.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+import {Moblie, Tablet, Desktop} from "../components/responsive";
+
+const color1 = "#344e41";
+const color2 = "#3a5a40";
+const color3 = "#588157";
+const color4 = "#a3b18a";
+const color5 = "#dad7cd";
+
+const VoyrosIntro = styled.div`
+    width: 100%;
+    height: 700px;
+    background: #344e41;
+`
 
 const MainHome = () => {
     return (
         <>
-            <h1>hello. welcome to the Voyros. </h1>
+            <VoyrosIntro></VoyrosIntro>
         </>
     )
 }
